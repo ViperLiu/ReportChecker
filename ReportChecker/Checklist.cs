8acde9db056944b61db92070142f93bddbd10a1d
ViperLiu <a0960330@gmail.com>
2020-05-12 10:10:02 +0800

    Add Checklist class

---
+++ ReportChecker/Checklist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportChecker
+{
+    class Checklist
+    {
+        private List<ITarget> _rulesToBeChecked = new List<ITarget>();
+
+        public void AddRule<T>() where T : ITarget, new()
+        {
+            T rule = new T();
+            _rulesToBeChecked.Add(rule);
+        }
+
+        public void CheckAll()
+        {
+            foreach(var target in _rulesToBeChecked)
+            {
+                target.Check();
+            }
+        }
+    }
+}
